9892f748b02a85e9080ac373f7dbe9fdb27fb210
Joe Greiner <jrgreiner115@gmail.com>
2018-03-13 01:38:00 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 function dwarfRollCall(dwarves) {
+var array = [];
+for (var i= 0; i<dwarves.length; i++) {array.push((i+1)+`. `+dwarves[i]+` `)}
+  return array.join("")
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  var arrayTwo= [];
+  for (var i=0; i<planeteerCalls.length; i++) {arrayTwo.push(planeteerCalls[i].toUpperCase()+`!`)}
+  return arrayTwo;
 }
 
 function longPlaneteerCalls(words) {
-}
+  for (var i=0; i<words.length; i++) {
+   if (words[i].length> 4) 
+ {return true}
+  else
+  {return false}
+ }}
 
 function findTheCheese (foods) {
+  for (var i = 0; i<foods.length; i++) {
+    if (foods[i] === "cheddar")
+    return foods[i]
+    else if (foods[i] === "gouda")
+    return "gouda";
+    else if (foods[i]=== "camembert")
+    return "camembert"}
+    
+    return "no cheese!"
+//  
+//   
+  
 }
